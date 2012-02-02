205bce7a9458be4f123a323b08b457f921d53cf1
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2012-02-02 00:38:03 +0000

    Decrease stack size from 500000 to 100000, which is the minimum that works on windows-x64
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@14409 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocAndor/st.cmd
+++ iocBoot/iocAndor/st.cmd
@@ -11,7 +11,7 @@ epicsEnvSet("XSIZE",  "2048")
 epicsEnvSet("YSIZE",  "2048")
 epicsEnvSet("NCHANS", "2048")
 
-andorCCDConfig("$(PORT)", -1, -1, "", 0, 500000)
+andorCCDConfig("$(PORT)", -1, -1, "", 0, 100000)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/andorCCD.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
