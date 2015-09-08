2b818f819f28d4ca9ef994e3a9374e3c1e4f688c
Mark Rivers <rivers@cars.uchicago.edu>
2015-09-08 14:46:43 -0500

    Moved commonPlugins.cmd from ADCore/iocBoot to ADExample/iocBoot

--- iocs/andorIOC/iocBoot/iocAndor/st.cmd
+++ iocs/andorIOC/iocBoot/iocAndor/st.cmd
@@ -36,7 +36,7 @@ NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
 dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=4200000")
 
 # Load all other plugins using commonPlugins.cmd
-< $(ADCORE)/iocBoot/commonPlugins.cmd
+< $(ADEXAMPLE)/iocBoot/commonPlugins.cmd
 set_requestfile_path("$(ADANDOR)/andorApp/Db")
 
 #asynSetTraceMask("$(PORT)",0,3)
